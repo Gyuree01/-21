1e224b7a4ebb4a54bf9a3311223d26eb195fa1fb
Gyuree01 <81613912+Gyuree01@users.noreply.github.com>
2021-05-02 23:35:27 +0900

    Add files via upload

--- 스윙 c++ 2_3.cpp
+++ 스윙 c++ 2_3.cpp
@@ -5,9 +5,9 @@ int main()
 {
 	int a = 10;
 	int* ptr; // 포인터 선언 
-	ptr = &a;
+	ptr = &a; // 포인터 초기화 
 
 	cout << "변수 a의 주소는: " << ptr << endl; 
-	cout << "a의 값은: " << a << endl; //a에 저장된 값을 출력
+	cout << "a의 값은: " << *ptr << endl; //a에 저장된 값을 출력
 	return 0;
 }
